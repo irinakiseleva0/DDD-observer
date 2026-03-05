5ac770e82a8210438e6a68f491d7a59dd1e28b05
irinakiseleva0 <irina.kiseleva@epita.fr>
2026-03-05 11:37:42 +0100

    fix logger with unique emitter

--- observers/logger.ts
+++ observers/logger.ts
@@ -1,8 +1,10 @@
-import { subscribe } from "../domain/events.js"
+import { subscribe, EMITTER } from "../domain/events.js"
 import { mockAuditLog } from "../services/mockAudit.js"
 
 export function registerLogger() {
   subscribe((event) => {
+    if (event.emitter !== EMITTER) return
+
     mockAuditLog(`EVENT: ${event.type}`, event)
   })
 }
